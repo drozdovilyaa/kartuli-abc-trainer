d6533aa5b2061e14f2984cf27779fc7970b90db2
Ilya Drozdov <ilya_drozdov@epam.com>
2025-11-10 21:21:21 +0400

    fix data

--- js/data.js
+++ js/data.js
@@ -1,82 +1,82 @@
 // Dictionary of Georgian-Russian letter pairs
 export const dictionary = [
     { georgian_letter: "ა", russian_letter: "а", comment: "" },
-    // { georgian_letter: "ბ", russian_letter: "б", comment: "" },
-    // { georgian_letter: "გ", russian_letter: "г", comment: "" },
-    // { georgian_letter: "დ", russian_letter: "д", comment: "" },
-    // { georgian_letter: "ე", russian_letter: "э", comment: "" },
-    // { georgian_letter: "ვ", russian_letter: "в", comment: "" },
-    // { georgian_letter: "ზ", russian_letter: "з", comment: "" },
-    // { georgian_letter: "თ", russian_letter: "тх", comment: "С придыханием" },
-    // { georgian_letter: "ი", russian_letter: "и", comment: "" },
-    // { georgian_letter: "კ", russian_letter: "к", comment: "Резкое, без выдоха" },
-    // { georgian_letter: "ლ", russian_letter: "л", comment: "" },
-    // { georgian_letter: "მ", russian_letter: "м", comment: "" },
-    // { georgian_letter: "ნ", russian_letter: "н", comment: "" },
-    // { georgian_letter: "ო", russian_letter: "о", comment: "" },
-    // { georgian_letter: "პ", russian_letter: "п", comment: "лёгкое, на выдохе" },
-    // { georgian_letter: "ჟ", russian_letter: "ж", comment: "" },
-    // { georgian_letter: "რ", russian_letter: "р", comment: "" },
-    // { georgian_letter: "ს", russian_letter: "с", comment: "" },
-    // { georgian_letter: "ტ", russian_letter: "т", comment: "" },
-    // { georgian_letter: "უ", russian_letter: "у", comment: "" },
-    // { georgian_letter: "ფ", russian_letter: "пф", comment: "" },
-    // { georgian_letter: "ქ", russian_letter: "кх", comment: "лёгкое, на выдохе" },
-    // { georgian_letter: "ღ", russian_letter: "гх", comment: "" },
-    // { georgian_letter: "ყ", russian_letter: "кх!", comment: "глубокое, без выдоха" },
-    // { georgian_letter: "შ", russian_letter: "ш", comment: "мягкое" },
-    // { georgian_letter: "ჩ", russian_letter: "ч", comment: "" },
-    // { georgian_letter: "ც", russian_letter: "ц", comment: "" },
-    // { georgian_letter: "ძ", russian_letter: "дз", comment: "" },
-    // { georgian_letter: "წ", russian_letter: "ц!", comment: "резкое, без выдоха" },
-    // { georgian_letter: "ჭ", russian_letter: "ч!", comment: "резкое, без выдоха" },
-    // { georgian_letter: "ხ", russian_letter: "х", comment: "глубокое" },
-    // { georgian_letter: "ჯ", russian_letter: "дж", comment: "совме­щённое" },
-    // { georgian_letter: "ჰ", russian_letter: "хх", comment: "лёгкое, на выдохе" }
+    { georgian_letter: "ბ", russian_letter: "б", comment: "" },
+    { georgian_letter: "გ", russian_letter: "г", comment: "" },
+    { georgian_letter: "დ", russian_letter: "д", comment: "" },
+    { georgian_letter: "ე", russian_letter: "э", comment: "" },
+    { georgian_letter: "ვ", russian_letter: "в", comment: "" },
+    { georgian_letter: "ზ", russian_letter: "з", comment: "" },
+    { georgian_letter: "თ", russian_letter: "тх", comment: "С придыханием" },
+    { georgian_letter: "ი", russian_letter: "и", comment: "" },
+    { georgian_letter: "კ", russian_letter: "к", comment: "Резкое, без выдоха" },
+    { georgian_letter: "ლ", russian_letter: "л", comment: "" },
+    { georgian_letter: "მ", russian_letter: "м", comment: "" },
+    { georgian_letter: "ნ", russian_letter: "н", comment: "" },
+    { georgian_letter: "ო", russian_letter: "о", comment: "" },
+    { georgian_letter: "პ", russian_letter: "п", comment: "лёгкое, на выдохе" },
+    { georgian_letter: "ჟ", russian_letter: "ж", comment: "" },
+    { georgian_letter: "რ", russian_letter: "р", comment: "" },
+    { georgian_letter: "ს", russian_letter: "с", comment: "" },
+    { georgian_letter: "ტ", russian_letter: "т", comment: "" },
+    { georgian_letter: "უ", russian_letter: "у", comment: "" },
+    { georgian_letter: "ფ", russian_letter: "пф", comment: "" },
+    { georgian_letter: "ქ", russian_letter: "кх", comment: "лёгкое, на выдохе" },
+    { georgian_letter: "ღ", russian_letter: "гх", comment: "" },
+    { georgian_letter: "ყ", russian_letter: "кх!", comment: "глубокое, без выдоха" },
+    { georgian_letter: "შ", russian_letter: "ш", comment: "мягкое" },
+    { georgian_letter: "ჩ", russian_letter: "ч", comment: "" },
+    { georgian_letter: "ც", russian_letter: "ц", comment: "" },
+    { georgian_letter: "ძ", russian_letter: "дз", comment: "" },
+    { georgian_letter: "წ", russian_letter: "ц!", comment: "резкое, без выдоха" },
+    { georgian_letter: "ჭ", russian_letter: "ч!", comment: "резкое, без выдоха" },
+    { georgian_letter: "ხ", russian_letter: "х", comment: "глубокое" },
+    { georgian_letter: "ჯ", russian_letter: "дж", comment: "совме­щённое" },
+    { georgian_letter: "ჰ", russian_letter: "хх", comment: "лёгкое, на выдохе" }
 ];
 
 // List of Georgian words for Mode 3
 export const georgianWords = [
     { word: "მამა", translation: "отец" },           // mama (father)
-    // { word: "დედა", translation: "мать" },           // deda (mother)
-    // { word: "სახლი", translation: "дом" },           // sakhli (house)
-    // { word: "წიგნი", translation: "книга" },         // ts'igni (book)
-    // { word: "ქალაქი", translation: "город" },        // k'alak'i (city)
-    // { word: "მთა", translation: "гора" },            // mta (mountain)
-    // { word: "თბილისი", translation: "Тбилиси" },    // tbilisi
-    // { word: "საქართველო", translation: "Грузия" },  // sak'art'velo (Georgia)
-    // { word: "წყალი", translation: "вода" },          // ts'q'ali (water)
-    // { word: "ძმა", translation: "брат" },            // dzma (brother)
-    // { word: "და", translation: "сестра" },           // da (sister)
-    // { word: "ბავშვი", translation: "ребенок" },      // bavshvi (child)
-    // { word: "კაცი", translation: "мужчина" },        // k'atsi (man)
-    // { word: "ქალი", translation: "женщина" },        // kali (woman)
-    // { word: "ძაღლი", translation: "собака" },        // dzaghli (dog)
-    // { word: "კატა", translation: "кошка" },          // k'at'a (cat)
-    // { word: "ხე", translation: "дерево" },           // khe (tree)
-    // { word: "ყვავილი", translation: "цветок" },      // q'vavili (flower)
-    // { word: "მზე", translation: "солнце" },          // mze (sun)
-    // { word: "მთვარე", translation: "луна" },         // mtvare (moon)
-    // { word: "ცა", translation: "небо" },             // tsa (sky)
-    // { word: "დღე", translation: "день" },            // dghe (day)
-    // { word: "ღამე", translation: "ночь" },           // ghame (night)
-    // { word: "სკოლა", translation: "школа" },         // sk'ola (school)
-    // { word: "მასწავლებელი", translation: "учитель" }, // masts'avlebeli (teacher)
-    // { word: "მეგობარი", translation: "друг" },       // megobari (friend)
-    // { word: "საჭმელი", translation: "еда" },         // sachмeli (food)
-    // { word: "პური", translation: "хлеб" },           // p'uri (bread)
-    // { word: "ღვინო", translation: "вино" },          // ghvino (wine)
-    // { word: "ხაჭაპური", translation: "хачапури" },  // khachap'uri (khachapuri)
-    // { word: "ხინკალი", translation: "хинкали" },    // khink'ali (khinkali)
-    // { word: "გზა", translation: "дорога" },          // gza (road)
-    // { word: "მანქანა", translation: "машина" },      // mankana (car)
-    // { word: "ფული", translation: "деньги" },        // p'uli (money)
-    // { word: "სახელი", translation: "имя" },          // sakheli (name)
-    // { word: "ენა", translation: "язык" },            // ena (language)
-    // { word: "მუსიკა", translation: "музыка" },       // musik'a (music)
-    // { word: "სიმღერა", translation: "песня" },       // simghera (song)
-    // { word: "ცეკვა", translation: "танец" },         // tsek'va (dance)
-    // { word: "სიყვარული", translation: "любовь" }     // siq'varuli (love)
+    { word: "დედა", translation: "мать" },           // deda (mother)
+    { word: "სახლი", translation: "дом" },           // sakhli (house)
+    { word: "წიგნი", translation: "книга" },         // ts'igni (book)
+    { word: "ქალაქი", translation: "город" },        // k'alak'i (city)
+    { word: "მთა", translation: "гора" },            // mta (mountain)
+    { word: "თბილისი", translation: "Тбилиси" },    // tbilisi
+    { word: "საქართველო", translation: "Грузия" },  // sak'art'velo (Georgia)
+    { word: "წყალი", translation: "вода" },          // ts'q'ali (water)
+    { word: "ძმა", translation: "брат" },            // dzma (brother)
+    { word: "და", translation: "сестра" },           // da (sister)
+    { word: "ბავშვი", translation: "ребенок" },      // bavshvi (child)
+    { word: "კაცი", translation: "мужчина" },        // k'atsi (man)
+    { word: "ქალი", translation: "женщина" },        // kali (woman)
+    { word: "ძაღლი", translation: "собака" },        // dzaghli (dog)
+    { word: "კატა", translation: "кошка" },          // k'at'a (cat)
+    { word: "ხე", translation: "дерево" },           // khe (tree)
+    { word: "ყვავილი", translation: "цветок" },      // q'vavili (flower)
+    { word: "მზე", translation: "солнце" },          // mze (sun)
+    { word: "მთვარე", translation: "луна" },         // mtvare (moon)
+    { word: "ცა", translation: "небо" },             // tsa (sky)
+    { word: "დღე", translation: "день" },            // dghe (day)
+    { word: "ღამე", translation: "ночь" },           // ghame (night)
+    { word: "სკოლა", translation: "школа" },         // sk'ola (school)
+    { word: "მასწავლებელი", translation: "учитель" }, // masts'avlebeli (teacher)
+    { word: "მეგობარი", translation: "друг" },       // megobari (friend)
+    { word: "საჭმელი", translation: "еда" },         // sachмeli (food)
+    { word: "პური", translation: "хлеб" },           // p'uri (bread)
+    { word: "ღვინო", translation: "вино" },          // ghvino (wine)
+    { word: "ხაჭაპური", translation: "хачапури" },  // khachap'uri (khachapuri)
+    { word: "ხინკალი", translation: "хинкали" },    // khink'ali (khinkali)
+    { word: "გზა", translation: "дорога" },          // gza (road)
+    { word: "მანქანა", translation: "машина" },      // mankana (car)
+    { word: "ფული", translation: "деньги" },        // p'uli (money)
+    { word: "სახელი", translation: "имя" },          // sakheli (name)
+    { word: "ენა", translation: "язык" },            // ena (language)
+    { word: "მუსიკა", translation: "музыка" },       // musik'a (music)
+    { word: "სიმღერა", translation: "песня" },       // simghera (song)
+    { word: "ცეკვა", translation: "танец" },         // tsek'va (dance)
+    { word: "სიყვარული", translation: "любовь" }     // siq'varuli (love)
 ];
 
 // List of Georgian phrases for Mode 5 (Russian phrase -> Select Georgian words)
